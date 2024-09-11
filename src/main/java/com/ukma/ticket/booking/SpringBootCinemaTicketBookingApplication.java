26e2fdf52ecc67470085104fa69c212627d26830
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-09-11 10:48:53 +0300

    initial commit

---
+++ src/main/java/com/ukma/ticket/booking/SpringBootCinemaTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ukma.ticket.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCinemaTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCinemaTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ukma/ticket/booking/SpringBootCinemaTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukma.ticket.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCinemaTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
